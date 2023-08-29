55e080cd621a4b8a24911dfccd4e131d97a7e927
kaiLuis <kaibedolla@icloud.com>
2023-08-29 06:01:23 -0600

    Annotating the code that was already there.

--- emulator.h
+++ emulator.h
@@ -1,30 +1,41 @@
 #pragma once
 
 #include <string>
-#include <memory>
+#include <memory> // Include the <memory> header for smart pointers
 
+// Forward declaration of the cxxopts::Options class
 namespace cxxopts {
     class Options;
 }
 
 namespace CLIEmulator {
 
+    // Declaration of the Emulator class
     class Emulator {
     public:
+        // Constructor that takes command-line arguments
         Emulator(int argc, char* argv[]);
+
+        // Function to run the emulator logic
         bool Run();
 
     private:
+        // Function to parse command-line options
         bool ParseCommandLineOptions();
+
+        // Function to validate parsed options
         bool ValidateOptions();
+
+        // Function to set up command-line options using cxxopts
         void SetupOptions(cxxopts::Options& app);
 
+        // Static function to handle signals (e.g., Ctrl+C)
         static void SignalHandler(int signal);
 
-        std::string emulatorType;
-        std::string emulatorOptions;
-        int argc;
-        char** argv;
+        std::string emulatorType; // Stores the emulator type
+        std::string emulatorOptions; // Stores emulator options
+        int argc; // Stores the number of command-line arguments
+        char** argv; // Stores the array of command-line argument strings
     };
 
 } // namespace CLIEmulator
